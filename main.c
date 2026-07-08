c718a743f1b000c3f3483a1846a404bef58406e0
Pen.Chanmonyka <penchanmonika@gmail.com>
2026-07-08 23:01:51 +0700

    third member (Monika) commit

--- main.c
+++ main.c
@@ -95,3 +95,69 @@ void add_student(struct Student students[])
 
     num_stu += new_count;
 }
+
+//========================
+//        Monika
+//=======================
+char getgrade(float average){
+    if (average >= 90)
+    {
+        return 'A';
+    }
+    else if (average >= 80)
+    {
+        return 'B';
+    }
+    else if (average >= 70)
+    {
+        return 'C';
+    }
+    else if (average >= 60)
+    {
+        return 'D';
+    }
+    else if (average >= 50)
+    {
+        return 'E';
+    }
+    else
+    {
+        return 'F';
+    }
+}
+
+void Individual(struct Student students[])
+{
+    int search_Id;
+    int found = 0;
+    printf("Enter Student ID : ");
+    scanf("%d", &search_Id);
+    if (num_stu == 0)
+    {
+        printf("No student yet!");
+    }
+
+    for (int i = 0; i < num_stu; i++)
+    {
+        if (search_Id == students[i].id)
+        {
+            found = 1;
+
+            printf("==================================\n");
+            printf("Student name : %s\n", students[i].name);
+            printf("ID : %d\n", students[i].id);
+            for (int j = 0; j < num_sub; j++)
+            {
+                printf("mark %d  : %.2f\n", j + 1, students[i].marks[j]);
+            }
+            printf("Total marks  : %.2f\n", students[i].total);
+            printf("Average      : %.2f\n", students[i].average);
+            printf("Grade        : %c", students[i].grade);
+            break;
+        }
+    }
+    if (!found)
+    {
+        printf("Student with id %d is not found\n", search_Id);
+    }
+}
